d0e388f79272a6624bb2e6402bc56d53e994581a
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-23 12:12:19 -0700

    Completed Milestone 16. Delete task being added

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 			<ul>
 			<?php require("includes/connect.php");//requiring connect.php file to connect to database-->
 				 $mysqli = new mysqli('localhost', 'root', 'root', 'todo-list');//connecting to database
-				$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";//query information, order by date and time
+				$query = "SELECT * FROM todo-list ORDER BY date ASC, time ASC";//query information, order by date and time
 				if ($result = $mysqli->query($query)) {//if result is a query
 					$numrows = $result->num_rows;//number of rows
 					if ($numrows>0) {//if number of rows is gretaer than 0
@@ -18,13 +18,13 @@
 							$task_id = $row['id'];//task id
 							$task_name = $row['task'];//and task name
 
-							echo "<li>
-							<span>'.$task_name'
-							";
+							echo '<li>
+							<span>' .$task_name. '</span>
+							<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
+							</li>';
 						}
 					}
 				}
-
 				?>
 			</ul>
 		</div>
