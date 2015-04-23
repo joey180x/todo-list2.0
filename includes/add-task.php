2c47828225acc1f030d35b9e268576e452d0388f
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-23 11:43:33 -0700

    Completed Milestone 12. Added task id

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,12 +1,22 @@
 <?php
- $task = strip_tags($_POST['task']);
- $date = date('Y-m-d');
- $time = date('H:i:s');
+ $task = strip_tags($_POST['task']);//stripiing  tags
+ $date = date('Y-m-d');//adding date
+ $time = date('H:i:s');//adding time
 
- include('connect.php');
+ include('connect.php');//adding connect.php
 
- $mysqli = new mysqli('localhost', 'root', 'root', 'todo-list');
- $mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
+ $mysqli = new mysqli('localhost', 'root', 'root', 'todo-list');//connecting to database
+ $mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");//inserting values into query
 
  $query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
+
+ if ($result = $mysqli->query($query)) {
+		while ($row = $result->fetch_assoc())
+			$task_id = $row['id'];
+			$task_name = $row['task'];
+			]
+	}
+
+	$mysqli->close();
+	
 ?>
